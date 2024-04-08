7633336db356719d3a30d3dbbfd6394a290ca066
Captain <mackenzie.me@icloud.com>
2024-04-08 19:55:30 +0700

    Take the exam

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,12 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalPrice = 27000;
+let discountPercentage = 25;
+let discountAmount = (totalPrice * discountPercentage) / 100;
+let finalPrice = totalPrice - discountAmount;
+
+console.log("Total Price:", totalPrice, "บาท");
+console.log("Discount Amount:", discountAmount, "บาท");
+console.log("Final Price after Discount:", finalPrice, "บาท");
